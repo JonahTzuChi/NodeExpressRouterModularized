70261b6ff47255d0e31b7bc17440e76ada1ec897
Jonah TzCh <115598785+JonahTzuChi@users.noreply.github.com>
2023-02-20 14:40:53 +0800

    Update API

--- scripts/client/index.js
+++ scripts/client/index.js
@@ -1,6 +1,6 @@
 var router = require("express").Router();
 
-router.get("/client/info", async (req, res, next)=>{
+router.get("/info", async (req, res, next)=>{
     res.send(JSON.stringify({
         message: "Thank you for checking out the client info"
     }));
--- scripts/product/index.js
+++ scripts/product/index.js
@@ -1,6 +1,6 @@
 var router = require("express").Router();
 
-router.get("/product/info", async (req, res, next)=>{
+router.get("/info", async (req, res, next)=>{
     res.send(JSON.stringify({
         message: "Thank you for checking out the product info"
     }));
